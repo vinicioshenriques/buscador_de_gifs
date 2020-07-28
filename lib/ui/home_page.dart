a306ef212e11cbbd80958158e3aec7c8805d4992
Vinicios Henriques <vinicioshenriques@hotmail.com>
2020-07-28 07:42:24 -0300

    Consulta na API finalizada, layout iniciado.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,17 +15,47 @@ class _HomePageState extends State<HomePage> {
   String _search;
   int _offset = 0;
 
-  _getGifs() async {
+  Future<Map> _getGifs() async {
     http.Response response;
     if (_search == null)
       response = await http.get(urlTrend);
     else
       response = await http.get(
           'https://api.giphy.com/v1/gifs/search?api_key=PE6IlQgRBU9lxiQ0YOhFcLhJREUzxhP8&q=${_search}&limit=20&offset=${_offset}&rating=g&lang=en');
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getGifs().then((value) => print(value));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Image.network(
+              'https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif'),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Pesquise Aqui',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(),
+            )
+          ],
+        ));
   }
 }
