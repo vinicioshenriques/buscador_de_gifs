571446e39d71f81c3d47f966014e266a5b410d1e
Vinicios Henriques <vinicioshenriques@hotmail.com>
2020-07-28 13:13:42 -0300

    Pesquisa e exibição de gifs pronto

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    ),
   ));
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
       response = await http.get(urlTrend);
     else
       response = await http.get(
-          'https://api.giphy.com/v1/gifs/search?api_key=PE6IlQgRBU9lxiQ0YOhFcLhJREUzxhP8&q=${_search}&limit=20&offset=${_offset}&rating=g&lang=en');
+          'https://api.giphy.com/v1/gifs/search?api_key=PE6IlQgRBU9lxiQ0YOhFcLhJREUzxhP8&q=${_search}&limit=19&offset=${_offset}&rating=g&lang=en');
 
     return json.decode(response.body);
   }
@@ -36,16 +36,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Image.network(
-              'https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif'),
-          centerTitle: true,
-        ),
+      appBar: AppBar(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            TextField(
+        title: Image.network(
+            'https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
               decoration: InputDecoration(
                 labelText: 'Pesquise Aqui',
                 labelStyle: TextStyle(
@@ -53,9 +55,82 @@ class _HomePageState extends State<HomePage> {
                 ),
                 border: OutlineInputBorder(),
               ),
-              style: TextStyle(),
-            )
-          ],
-        ));
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: _getCount(snapshot.data['data']),
+      itemBuilder: (context, index) {
+        if (_search == null || index < snapshot.data['data'].length) {
+          return GestureDetector(
+            onTap: () {},
+            child: Image.network(
+              snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        } else {
+          return Container(
+            child: GestureDetector(
+              onTap: () {},
+              child: Column(
+                children: [
+                  Icon(Icons.add),
+                ],
+              ),
+            ),
+          );
+        }
+      },
+    );
   }
 }
